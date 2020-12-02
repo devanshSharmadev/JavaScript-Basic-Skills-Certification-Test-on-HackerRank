3c5388ea92b653629eebad8cafc94987644fe974
Devansh Sharma <64777208+devanshSharmadev@users.noreply.github.com>
2020-12-02 15:25:19 +0530

    Create joinedLogger.js

---
+++ joinedLogger.js
@@ -0,0 +1,16 @@
+function joinedLogger(level, sep) {
+    var s=[]
+    return function(...msg)
+    {
+        msg.forEach(arg => {
+            if(arg.level>=level)
+            {
+                s.push(arg.text)
+            }
+        });
+        var ans=String(s.join(sep))
+        ws.write(ans)
+    
+    }
+  // write your code here
+}
